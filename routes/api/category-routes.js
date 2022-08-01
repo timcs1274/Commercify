7b681f4e829e9db04bd1e07b6d7230b4ad38d97b
timcs <timcs1274@gmail.com>
2022-08-01 19:51:50 -0400

    finished 2nd get route 'findone'

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -20,8 +20,28 @@ router.get('/', (req, res) => {
 });
 
 router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+  Category.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [
+      {
+        model: Product,
+        attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+      }
+    ]
+  })
+  .then(dbCategoryData => {
+    if (!dbCategoryData) {
+      res.status(404).json({ message: 'There is no category assigned to this id' });
+      return;
+    }
+    res.json(dbCategoryData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.post('/', (req, res) => {
