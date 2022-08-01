e4f068cfb7c80d6560b77dead3fa9ef09ca8328c
timcs <timcs1274@gmail.com>
2022-08-01 19:41:39 -0400

    added delete route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -32,7 +32,7 @@ router.put('/:id', (req, res) => {
   })
   .then(dbCategoryData => {
     if (!dbCategoryData[0]) {
-    res.status(404).json({ message: 'There is no category assigned to this id'});
+    res.status(404).json({ message: 'There is no category assigned to this id' });
     return;
   }
     res.json(dbCategoryData);
@@ -44,7 +44,22 @@ router.put('/:id', (req, res) => {
 });
 
 router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(dbCategoryData => {
+    if (!dbCategoryData) {
+      res.status(404).json({ message: 'There is no category assigned to this id' });
+      return;
+    }
+    res.json(dbCategoryData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 module.exports = router;
