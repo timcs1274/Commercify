b0e93a6a50839a6203319b4f9cb76b40dee6f864
timcs <timcs1274@gmail.com>
2022-08-01 20:20:58 -0400

    added finall product route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -5,8 +5,23 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // get all products
 router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+  Product.findAll({
+    include: [
+      {
+        model: Category,
+        attributes: ['id', 'category_name']
+      },
+      {
+        model: Tag,
+        attributes: ['id', 'tag_name']
+      }
+    ]
+  })
+  .then(dbProductData => res.json(dbProductData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 // get one product
